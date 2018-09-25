d827345eddac73eafb5b3d424ef9edcaa372f667
Antons Lelis <3lelia74@solent.ac.uk>
2018-09-25 10:38:57 +0100

    chicking in my first example

---
+++ myPracticeCourseWork/week1/example1/src/test/java/org/antonslelis/com504/ood/MyTestClassLog4jTest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.antonslelis.com504.ood;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author 3lelia74
+ */
+public class MyTestClassLog4jTest {
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
